bac0220338292f3d4086b454f2f4d2156f540e00
Bart Huttinga <bart@qaraqter.nl>
2014-07-03 12:05:32 +0200

    vendors back to vendor dir

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 date_default_timezone_set('Europe/Amsterdam');
 
-require_once 'source/vendors/autoload.php';
+require_once 'vendor/autoload.php';
 
 $loader = new \Twig_Loader_Filesystem('twig');
 $twig = new \Twig_Environment($loader, array(
